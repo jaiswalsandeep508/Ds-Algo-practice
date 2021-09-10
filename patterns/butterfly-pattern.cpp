d3577e1719b2c5510edba3db18de180bfaee114a
sandeep <jaiswalsandeep508@gmail.com>
2021-09-10 15:31:52 +0530

    pattern butterfly

---
+++ patterns/butterfly-pattern.cpp
@@ -0,0 +1,58 @@
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+    int n,i,j,space;
+
+    cout<<"enter any integer: ";
+    cin>>n;
+    cout<<endl;
+    
+   
+    for(i=1;i<=n;i++)
+    {
+        for(j=1;j<=i;j++)
+        {
+            cout<<"* ";
+        }
+        
+        space=2*n-2*i;
+        for(j=1;j<=space;j++)
+        {
+            cout<<"  ";
+        }
+
+         for(j=1;j<=i;j++)
+        {
+            cout<<"* ";
+        }
+        
+
+        cout<<endl;
+    }
+    for(i=n;i>=1;i--)
+    {
+        for(j=1;j<=i;j++)
+        {
+            cout<<"* ";
+        }
+        
+        space=2*n-2*i;
+        for(j=1;j<=space;j++)
+        {
+            cout<<"  ";
+        }
+
+         for(j=1;j<=i;j++)
+        {
+            cout<<"* ";
+        }
+        
+
+        cout<<endl;
+    }
+
+return 0;
+}
