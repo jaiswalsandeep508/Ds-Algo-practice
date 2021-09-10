e1ef78fc395137aa501a6da4afd39177713dc186
sandeep <jaiswalsandeep508@gmail.com>
2021-09-10 14:31:51 +0530

    pattern inverted half pyiramid

---
+++ patterns/inverted-half-pyramid.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+    int n,i,j;
+
+    cout<<"enter any integer: "<<endl;
+    cin>>n;
+    
+   
+    for(i=n;i>=1;i--)
+    {
+        for(j=1;j<=i;j++)
+        {
+           cout<<"*";
+        }
+        cout<<endl;
+    }
+
+return 0;
+
+}
