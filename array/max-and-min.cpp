292246a33e59aa55153f1e58ee5788a545f7cb99
sandeep <jaiswalsandeep508@gmail.com>
2021-09-14 15:20:49 +0530

    array max and min

---
+++ array/max-and-min.cpp
@@ -0,0 +1,30 @@
+#include<iostream>
+#include<climits>
+
+using namespace std;
+
+int main()
+{
+    int n,i,array[n];
+    int maxno=INT_MIN;
+    int minno=INT_MAX;
+
+    cout<<"Enter the value of 'n': ";
+    cin>>n;
+    cout<<endl;
+    for(i=1;i<n;i++)
+    {
+        cin>>array[i];
+    }
+
+    for(i=0;i<n;i++)
+    {
+        
+       maxno=max(maxno,array[i]);
+       minno=min(minno,array[i]);
+    }
+
+    cout<<"Max Number: "<<maxno<<endl<<"Min Number: "<<minno;
+
+    return 0;
+}
